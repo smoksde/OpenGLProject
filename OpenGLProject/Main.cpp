56fc316a4b6982872613e8f7d21ca9eec1abd224
oehlandt <oehlandt@campus.tu-berlin.de>
2022-08-27 21:34:30 +0200

    Projektdateien hinzufügen.

---
+++ OpenGLProject/Main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+
+//Vertices coordinates
+GLfloat vertices[] = {
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper center corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+};
+
+GLuint indices[] = {
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, //Lower right triangle
+	5, 4, 1 // Upper triangle
+};
+
+int main() {
+	
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// so that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	
+
+	// Create a new GLFWwindow object of 800 by 800 pixels, naming it "OpenGLProject"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGLProject", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the Window
+	// In this case the whole window is used
+	glViewport(0, 0, 800, 800);
+
+	Shader shaderProgram("default.vert", "default.frag");
+
+	VAO VAO1;
+	VAO1.Bind();
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkVBO(VBO1, 0);
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window)) {
+		// Specifiy the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		shaderProgram.Activate();
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+
+	// Delete Window before exiting
+	glfwDestroyWindow(window);
+	// Terminate whole GLFW process
+	glfwTerminate();
+	return 0;
+}
